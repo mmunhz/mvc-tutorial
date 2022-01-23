daa3518ed081783dc7f87b5e32deaba5ed5f12a2
mmunhz <mmunhz.ufabc@gmail.com>
2022-01-23 11:45:26 -0300

    Create Task model

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const { engine } = require('express-handlebars')
+
+const app = express()
+
+const conn = require('./db/conn')
+
+const Task = require('./models/Task')
+
+app.engine('handlebars', engine())
+app.set('view engine', 'handlebars')
+
+app.use(
+    express.urlencoded({
+        extended: true
+    })
+)
+
+app.use(express.json())
+
+app.use(express.static('public'))
+
+conn.sync()
+    .then(() => {
+        app.listen(3000)
+    })
+    .catch((error) => console.log(error))
